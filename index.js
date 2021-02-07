01c8410161d8af4ff758999b2629a4ab06a3decf
xosemanolo <xose.manolo.rodriguez@gmail.com>
2021-02-07 11:32:53 -0500

    wsjScraper

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-import { getHTML } from "./lib/scraperSA";
+
 import { getWSJ } from "./lib/scraperWSJ";
 import { twitterWSJDataTweets } from "./lib/twitterWSJDataTweets";
 import { getFearAndGreedHTML, getFearAndGreedData } from "./lib/fearandgreed";
-import { downloadPDF } from "./lib/ARKholdings";
-import { scraperSARocketStockNews } from "./lib/scraperSARocketStockNews";
+
 
 async function go() {
-  getFearAndGreedData(
-    await getFearAndGreedHTML("https://money.cnn.com/data/fear-and-greed/")
-  );
+  // getFearAndGreedData(
+  //   await getFearAndGreedHTML("https://money.cnn.com/data/fear-and-greed/")
+  // );
   await getWSJ("https://www.wsj.com/market-data/stocks?mod=nav_top_subsection");
-  await twitterWSJDataTweets.twitterCredentials();
   await twitterWSJDataTweets.issuesTweet();
   await twitterWSJDataTweets.newHighLow();
   await twitterWSJDataTweets.indexVolumes();
   await twitterWSJDataTweets.percentNYSE();
 
-  // await getHTML("https://seekingalpha.com/");
-  // await scraperSARocketStockNews();
-  // await downloadPDF();
+ 
 }
 
 go();
